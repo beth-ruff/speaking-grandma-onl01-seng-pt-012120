57fe8969eec192d6cfb35f2b2c11c547bd55f234
Beth Ruff <beth.ruff2@gmail.com>
2020-01-24 23:32:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,6 @@
 def speak_to_grandma(phrase)
-  if phrase == "I love you grandma".upcase
-    puts "I LOVE YOU "
+  if phrase == "I love you grandma!".upcase
+    puts "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == "I love you granmda!"
+    puts ""
 end
