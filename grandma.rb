95d05769f6471fcf31d9098e824a813d979db6fe
Beth Ruff <beth.ruff2@gmail.com>
2020-01-24 23:35:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,5 +7,6 @@ def speak_to_grandma(phrase)
     puts "HUH?!  SPEAK UP, SONNY!"
   elseif phrase == "what did you eat today?".upcase
     puts "NO, NOT SINCE 1938!"
-  else phrase ==
+  else phrase == "what?".upcase
+    puts "NO, NOT SINCE 1938!"
 end
