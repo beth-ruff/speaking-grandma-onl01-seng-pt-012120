846c4f75916b42be1d8b8559e1e74b31ab9aab2f
Beth Ruff <beth.ruff2@gmail.com>
2020-01-24 23:30:57 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,12 +1,3 @@
-def speak_to_grandma("I love you grandma")
-if speak_to_grandma == "#{I love you grandma}".upcase
- puts "I LOVE YOU TOO PUMPKIN!"
-elsif speak_to_grandma == "#{I love you grandma}".upcase
-  puts "HUH?! SPEAK UP, SONNY!"
-elsif speak_to_grandma == "#{I love you grandma}"
-  puts "HUH?! SPEAK UP, SONNY!"
-elsif speak_to_grandma == "Hi".upcase
-  puts "NO, NOT SINCE 1938!"
-else speak_to_grandma == "Are you sure?".upcase
-  puts "NO, NOT SINCE 1938!"
+def speak_to_grandma(phrase)
+  
 end
