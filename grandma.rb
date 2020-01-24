6d90315688658ea550759d9af5f297de6ffc9ca7
Beth Ruff <beth.ruff2@gmail.com>
2020-01-24 23:34:03 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,5 +3,7 @@ def speak_to_grandma(phrase)
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == "Hi Nana, how are you?"
     puts "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == 
+  elsif phrase == "Hi!"
+    puts "HUH?!  SPEAK UP, SONNY!"
+  elseif phrase == 
 end
